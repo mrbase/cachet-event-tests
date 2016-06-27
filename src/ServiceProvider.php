bdd94b9631f9e1755daac980fd8b067126411802
Ulrik Nielsen <me@ulrik.co>
2016-06-27 14:57:39 +0200

    updated test

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,14 +3,28 @@
 namespace Mrbase\CachetEventTests;
 
 use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
-use Illuminate\Foundation\Support\Providers\EventServiceProvider;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use Mrbase\CachetEventTests\Handlers;
 
-class ServiceProvider extends EventServiceProvider
+class ServiceProvider extends BaseServiceProvider
 {
-    protected $listen = [
-        'CachetHQ\Cachet\Bus\Events\Incident\IncidentWasReportedEvent'   => [Handlers\IncidentWasReported::class],
-        'CachetHQ\Cachet\Bus\Events\Incident\IncidentWasUpdatedEvent'    => [Handlers\IncidentWasUpdated::class],
-        'CachetHQ\Cachet\Bus\Events\Component\ComponentWasUpdatedEvent>' => [Handlers\ComponentWasUpdated::class],
-    ];
+#    protected $listen = [
+#        'CachetHQ\Cachet\Bus\Events\Incident\IncidentWasReportedEvent'  => [Handlers\IncidentWasReported::class],
+#        'CachetHQ\Cachet\Bus\Events\Incident\IncidentWasUpdatedEvent'   => [Handlers\IncidentWasUpdated::class],
+#        'CachetHQ\Cachet\Bus\Events\Component\ComponentWasUpdatedEvent' => [Handlers\ComponentWasUpdated::class],
+#    ];
+
+    public function boot()
+    {
+        $events->listen('CachetHQ\Cachet\Bus\Events\Incident\IncidentWasReportedEvent', function () {
+            \Log::debug('IncidentWasReportedEvent event called ...');
+        });
+        $events->listen('CachetHQ\Cachet\Bus\Events\Incident\IncidentWasUpdatedEvent', function () {
+            \Log::debug('IncidentWasUpdatedEvent event called ...');
+        });
+        $events->listen('CachetHQ\Cachet\Bus\Events\Component\ComponentWasUpdatedEvent', function () {
+            \Log::debug('ComponentWasUpdatedEvent event called ...');
+        });
+    }
+
 }
